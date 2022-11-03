1460ee677b0a64a44cd3611fda93f36fde51bc1c
ngrok.cc内网穿透 <327388905@qq.com>
2022-11-03 13:18:43 +0800

    fix: 更新v3接口获取签名

--- controller/Txcos.php
+++ controller/Txcos.php
@@ -11,180 +11,129 @@ use beacon\core\Request;
 
 class Txcos extends Controller
 {
-    /**
-     * 参数转换
-     * @param $obj
-     * @return string
-     */
-    private static function toParam($obj): string
+
+    // 临时密钥计算样例
+    private function _hex2bin($data)
+    {
+        $len = strlen($data);
+        return pack("H" . $len, $data);
+    }
+
+    // obj 转 query string
+    private function json2str($obj, $notEncode = false)
     {
         ksort($obj);
         $arr = array();
+        if (!is_array($obj)) {
+            throw new Exception($obj + " must be a array");
+        }
         foreach ($obj as $key => $val) {
-            $arr[] = $key . '=' . $val;
+            array_push($arr, $key . '=' . ($notEncode ? $val : rawurlencode($val)));
         }
         return join('&', $arr);
     }
 
-    /**
-     * 获取签名
-     * @param $opt
-     * @param $key
-     * @param $method
-     * @return string
-     */
-    private static function getSignature($opt, $key, $method): string
+    // 计算临时密钥用的签名
+    private function getSignature($opt, $key, $method, $config)
     {
-        $domain = Config::get('txcos.domain', '');
-        $formatString = $method . $domain . '/v2/index.php?' . self::toParam($opt);
+        $formatString = $method . $config['domain'] . '/?' . $this->json2str($opt, 1);
         $sign = hash_hmac('sha1', $formatString, $key);
-        return base64_encode(hex2bin($sign));
+        $sign = base64_encode($this->_hex2bin($sign));
+        return $sign;
     }
 
-    /**
-     * 获得临时钥匙
-     * @return array
-     */
-    private static function getTempKeys(): array
+    // v2接口的key首字母小写，v3改成大写，此处做了向下兼容
+    private function backwardCompat($result)
     {
-        $bucket = Config::get('txcos.bucket', '');
-        $allowPrefix = Config::get('txcos.allow_prefix', '_ALLOW_DIR_/*');
-        $region = Config::get('txcos.region');
-        $secretId = Config::get('txcos.secret_id');
-        $secretKey = Config::get('txcos.secret_key');
-        $proxy = Config::get('txcos.proxy');
-        $signUrl = Config::get('txcos.sign_url');
-
-        // 判断是否修改了 AllowPrefix
-        if ($allowPrefix === '_ALLOW_DIR_/*') {
-            return array('error' => '请修改 AllowPrefix 配置项，指定允许上传的路径前缀');
+        if (!is_array($result)) {
+            throw new Exception($result + " must be a array");
         }
-        $ShortBucketName = substr($bucket, 0, strripos($bucket, '-'));
-        $AppId = substr($bucket, 1 + strripos($bucket, '-'));
-        $resource = [];
-        $resource[] = 'qcs::cos:' . $region . ':uid/' . $AppId . ':prefix//' . $AppId . '/' . $ShortBucketName . '/';
-        if (is_array($allowPrefix)) {
-            foreach ($allowPrefix as $prefix) {
-                $resource[] = 'qcs::cos:' . $region . ':uid/' . $AppId . ':prefix//' . $AppId . '/' . $ShortBucketName . '/' . $prefix;
+        $compat = array();
+        foreach ($result as $key => $value) {
+            if (is_array($value)) {
+                $compat[lcfirst($key)] = $this->backwardCompat($value);
+            } elseif ($key == 'Token') {
+                $compat['sessionToken'] = $value;
+            } else {
+                $compat[lcfirst($key)] = $value;
             }
+        }
+        return $compat;
+    }
+
+    // 获取临时密钥
+    private function getTempKeys($config)
+    {
+        if (array_key_exists('bucket', $config)) {
+            $ShortBucketName = substr($config['bucket'], 0, strripos($config['bucket'], '-'));
+            $AppId = substr($config['bucket'], 1 + strripos($config['bucket'], '-'));
+        }
+        if (array_key_exists('policy', $config)) {
+            $policy = $config['policy'];
         } else {
-            $resource[] = 'qcs::cos:' . $region . ':uid/' . $AppId . ':prefix//' . $AppId . '/' . $ShortBucketName . '/' . $allowPrefix;
-        }
-        $policy = array(
-            'version' => '2.0',
-            'statement' => array(
-                array(
-                    'action' => array(
-                        // // 这里可以从临时密钥的权限上控制前端允许的操作
-                        //  'name/cos:*', // 这样写可以包含下面所有权限
-                        // // 列出所有允许的操作
-                        // // ACL 读写
-                        // 'name/cos:GetBucketACL',
-                        // 'name/cos:PutBucketACL',
-                        // 'name/cos:GetObjectACL',
-                        // 'name/cos:PutObjectACL',
-                        // // 简单 Bucket 操作
-                        // 'name/cos:PutBucket',
-                        // 'name/cos:HeadBucket',
-                        // 'name/cos:GetBucket',
-                        // 'name/cos:DeleteBucket',
-                        // 'name/cos:GetBucketLocation',
-                        // // Versioning
-                        // 'name/cos:PutBucketVersioning',
-                        // 'name/cos:GetBucketVersioning',
-                        // // CORS
-                        // 'name/cos:PutBucketCORS',
-                        // 'name/cos:GetBucketCORS',
-                        // 'name/cos:DeleteBucketCORS',
-                        // // Lifecycle
-                        // 'name/cos:PutBucketLifecycle',
-                        // 'name/cos:GetBucketLifecycle',
-                        // 'name/cos:DeleteBucketLifecycle',
-                        // // Replication
-                        // 'name/cos:PutBucketReplication',
-                        // 'name/cos:GetBucketReplication',
-                        // 'name/cos:DeleteBucketReplication',
-                        // // 删除文件
-                        // 'name/cos:DeleteMultipleObject',
-                        // 'name/cos:DeleteObject',
-                        // 简单文件操作
-                        'name/cos:PutObject',
-                        'name/cos:PostObject',
-                        'name/cos:AppendObject',
-                        'name/cos:GetObject',
-                        'name/cos:HeadObject',
-                        'name/cos:OptionsObject',
-                        'name/cos:PutObjectCopy',
-                        'name/cos:PostObjectRestore',
-                        // 分片上传操作
-                        'name/cos:InitiateMultipartUpload',
-                        'name/cos:ListMultipartUploads',
-                        'name/cos:ListParts',
-                        'name/cos:UploadPart',
-                        'name/cos:CompleteMultipartUpload',
-                        'name/cos:AbortMultipartUpload',
-                    ),
-                    'effect' => 'allow',
-                    'principal' => array('qcs' => array('*')),
-                    'resource' => $resource
+            $policy = array(
+                'version' => '2.0',
+                'statement' => array(
+                    array(
+                        'action' => $config['allowActions'],
+                        'effect' => 'allow',
+                        'principal' => array('qcs' => array('*')),
+                        'resource' => array(
+                            'qcs::cos:' . $config['region'] . ':uid/' . $AppId . ':prefix//' . $AppId . '/' . $ShortBucketName . '/' . $config['allowPrefix']
+                        )
+                    )
                 )
-            )
-        );
-        $policyStr = str_replace('\\/', '/', json_encode($policy));
-
-        // 有效时间小于 30 秒就重新获取临时密钥，否则使用缓存的临时密钥
-        $tempKeysCache = Request::getSession('tempKeysCache', null);
-        if (!empty($tempKeysCache)) {
-            if (isset($tempKeysCache['expiredTime']) && isset($tempKeysCache['policyStr']) && $tempKeysCache['expiredTime'] - time() > 30 && $tempKeysCache['policyStr'] === $policyStr) {
-                return $tempKeysCache;
-            }
+            );
         }
+        $policyStr = str_replace('\\/', '/', json_encode($policy));
         $Action = 'GetFederationToken';
         $Nonce = rand(10000, 20000);
-        $Timestamp = time() - 1;
-        $Method = 'GET';
+        $Timestamp = time();
+        $Method = 'POST';
         $params = array(
-            'Action' => $Action,
-            'Nonce' => $Nonce,
-            'Region' => '',
-            'SecretId' => $secretId,
+            'SecretId' => $config['secretId'],
             'Timestamp' => $Timestamp,
-            'durationSeconds' => 7200,
-            'name' => '',
-            'policy' => $policyStr
+            'Nonce' => $Nonce,
+            'Action' => $Action,
+            'DurationSeconds' => $config['durationSeconds'],
+            'Version' => '2018-08-13',
+            'Name' => 'cos',
+            'Region' => 'ap-guangzhou',
+            'Policy' => urlencode($policyStr)
         );
-        $params['Signature'] = urlencode(self::getSignature($params, $secretKey, $Method));
-        $url = $signUrl . '?' . self::toParam($params);
-
+        $params['Signature'] = $this->getSignature($params, $config['secretKey'], $Method, $config);
+        $url = $config['url'];
         $ch = curl_init($url);
-        if (!empty($proxy)) {
-            curl_setopt($ch, CURLOPT_PROXY, $proxy);
+        if (array_key_exists('proxy', $config)) {
+            $config['proxy'] && curl_setopt($ch, CURLOPT_PROXY, $config['proxy']);
         }
         curl_setopt($ch, CURLOPT_HEADER, 0);
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $this->json2str($params));
         $result = curl_exec($ch);
         if (curl_errno($ch)) $result = curl_error($ch);
         curl_close($ch);
         $result = json_decode($result, 1);
-        if (isset($result['data'])) {
-            $result = $result['data'];
+        if (isset($result['Response'])) {
+            $result = $result['Response'];
+            $result['startTime'] = $result['ExpiredTime'] - $config['durationSeconds'];
         }
-        $tempKeysCache = $result;
-        $tempKeysCache['policyStr'] = $policyStr;
-        Request::setSession('tempKeysCache', $tempKeysCache);
+        $result = $this->backwardCompat($result);
         return $result;
     }
 
-    // 计算 COS API 请求用的签名
-    private static function getAuthorization($keys, $method, $pathname): string
+    private function getAuthorization($keys, $method, $pathname): string
     {
         // 获取个人 API 密钥 https://console.qcloud.com/capi
         $SecretId = $keys['credentials']['tmpSecretId'];
         $SecretKey = $keys['credentials']['tmpSecretKey'];
+
         // 整理参数
-        $query = array();
-        $headers = array();
+        $query = [];
+        $headers = [];
         $method = strtolower($method ? $method : 'get');
         $pathname = $pathname ? $pathname : '/';
         substr($pathname, 0, 1) != '/' && ($pathname = '/' . $pathname);
@@ -198,7 +147,7 @@ class Txcos extends Controller
 
         function obj2str($obj)
         {
-            $list = array();
+            $list = [];
             $keyList = getObjectKeys($obj);
             $len = count($keyList);
             for ($i = 0; $i < $len; $i++) {
@@ -211,12 +160,11 @@ class Txcos extends Controller
         }
 
         // 签名有效起止时间
-        $now = time() - 1;
+        $now = time();
         $expired = $now + 600; // 签名过期时刻，600 秒后
         // 要用到的 Authorization 参数列表
         $qSignAlgorithm = 'sha1';
         $qAk = $SecretId;
-        $qSignTime = $now . ';' . $expired;
         $qKeyTime = $now . ';' . $expired;
         $qHeaderList = strtolower(implode(';', getObjectKeys($headers)));
         $qUrlParamList = strtolower(implode(';', getObjectKeys($query)));
@@ -224,49 +172,75 @@ class Txcos extends Controller
         // 步骤一：计算 SignKey
         $signKey = hash_hmac("sha1", $qKeyTime, $SecretKey);
         // 步骤二：构成 FormatString
-        $formatString = implode("\n", array(strtolower($method), $pathname, obj2str($query), obj2str($headers), ''));
-        //header('x-test-method', $method);
-        //header('x-test-pathname', $pathname);
+        $formatString = implode("\n", [strtolower($method), $pathname, obj2str($query), obj2str($headers), '']);
         // 步骤三：计算 StringToSign
-        $stringToSign = implode("\n", array('sha1', $qSignTime, sha1($formatString), ''));
+        $stringToSign = implode("\n", ['sha1', $qKeyTime, sha1($formatString), '']);
         // 步骤四：计算 Signature
         $qSignature = hash_hmac('sha1', $stringToSign, $signKey);
         // 步骤五：构造 Authorization
-        $authorization = implode('&', array(
+        return implode('&', [
             'q-sign-algorithm=' . $qSignAlgorithm,
             'q-ak=' . $qAk,
-            'q-sign-time=' . $qSignTime,
+            'q-sign-time=' . $qKeyTime,
             'q-key-time=' . $qKeyTime,
             'q-header-list=' . $qHeaderList,
             'q-url-param-list=' . $qUrlParamList,
             'q-signature=' . $qSignature
-        ));
-        return $authorization;
+        ]);
     }
 
-    /**
-     * @param string $method
-     * @param string $pathname
-     * @return array
-     */
     #[Method(act: 'auth', method: Method::GET | Method::POST, contentType: 'json')]
-    public function auth(string $method = 'get', string $pathname = '/')
+    public function auth(string $method = 'post', string $pathname = '/')
     {
+        // 配置参数
+        $config = array(
+            'url' => 'https://sts.tencentcloudapi.com/',
+            'domain' => 'sts.tencentcloudapi.com',
+            'proxy' => '',
+            'secretId' => 'AKIDZBW2FHA7rnTN7elKablfmbATZS5Vovvh', // 固定密钥
+            'secretKey' => 'e4A0f6ex1qc2LGvnQmsjKguyvc3FAtNg', // 固定密钥
+            'bucket' => 'bz-test-1251004022', // 换成你的 bucket
+            'region' => 'ap-guangzhou', // 换成 bucket 所在园区
+            'durationSeconds' => 1800, // 密钥有效期
+            // 允许操作（上传）的对象前缀，可以根据自己网站的用户登录态判断允许上传的目录，例子： user1/* 或者 * 或者a.jpg
+            // 请注意当使用 * 时，可能存在安全风险，详情请参阅：https://cloud.tencent.com/document/product/436/40265
+            'allowPrefix' => '_ALLOW_DIR_/*',
+            // 密钥的权限列表。简单上传和分片需要以下的权限，其他权限列表请看 https://cloud.tencent.com/document/product/436/31923
+            'allowActions' => [
+                // 所有 action 请看文档 https://cloud.tencent.com/document/product/436/31923
+                // 简单文件操作
+                'name/cos:PutObject',
+                'name/cos:PostObject',
+                'name/cos:AppendObject',
+                'name/cos:GetObject',
+                'name/cos:HeadObject',
+                'name/cos:OptionsObject',
+                'name/cos:PutObjectCopy',
+                'name/cos:PostObjectRestore',
+                // 分片上传操作
+                'name/cos:InitiateMultipartUpload',
+                'name/cos:ListMultipartUploads',
+                'name/cos:ListParts',
+                'name/cos:UploadPart',
+                'name/cos:CompleteMultipartUpload',
+                'name/cos:AbortMultipartUpload',
+            ]
+        );
         // 获取临时密钥，计算签名
-        $tempKeys = self::getTempKeys();
+        $tempKeys = $this->getTempKeys($config);
         if ($tempKeys && isset($tempKeys['credentials'])) {
-            $data = array(
-                'Authorization' => self::getAuthorization($tempKeys, $method, $pathname),
+            $data = [
+                'Authorization' => $this->getAuthorization($tempKeys, $method, $pathname),
                 'XCosSecurityToken' => $tempKeys['credentials']['sessionToken'],
-            );
+            ];
         } else {
-            $data = array('error' => $tempKeys);
+            $data = ['error' => $tempKeys];
         }
         // 返回数据给前端
-        //header('Content-Type: application/json');
+        header('Content-Type: application/json');
         header('Access-Control-Allow-Origin: *'); // 这里修改允许跨域访问的网站
         header('Access-Control-Allow-Headers: origin,accept,content-type');
-        return $data;
+        echo json_encode($data);
     }
 
 
